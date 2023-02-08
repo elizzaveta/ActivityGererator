30e84d07aca093da6271e44b0e047e7bdc62c390
elizzaveta <y.timchenko-2025@kpi.com>
2023-02-08 15:22:43 +0200

    Added styles for Data tab in Settings

--- src/components/Settings/Data/DataTab.vue
+++ src/components/Settings/Data/DataTab.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <h1>Data</h1>
+    <p v-if="numOfFavoriteActivities===0">You don't have any saved activities.</p>
+    <p v-if="numOfFavoriteActivities!==0">You have
+      {{numOfFavoriteActivities}}
+      activit<span v-if="numOfFavoriteActivities===1">y</span>
+      <span v-if="numOfFavoriteActivities!==1">ies</span>
+      saved.</p>
     <h2>Clear data</h2>
-    <p>You have {{numOfFavoriteActivities}} activities saved.</p>
+    <p>Delete all saved activities</p>
     <button @click="handleClearButtonClick" :disabled="isDisabled">Clear</button>
     <h2>Export data</h2>
-    <p>Export your favorites to a file</p>
-    <button @click="handleExportButtonClick">Export</button>
-    <select v-model="selected">
+    <p>Export saved activities to a file</p>
+    <button @click="handleExportButtonClick" :disabled="isDisabled">Export</button>
+    <select v-model="selected"  :disabled="isDisabled">
       <option v-for="option in options" v-bind:value="option">
         {{ option }}
       </option>
@@ -30,7 +36,6 @@ export default {
   },
   methods:{
     handleClearButtonClick(){
-
       let confirmed = confirm("Are you sure, you want to delete all favorites?")
       if(confirmed) {
         this.clearUserData();
@@ -53,11 +58,22 @@ export default {
       if(this.numOfFavoriteActivities !== 0){ this.isDisabled = false}
 
     }
-    console.log(this.numOfFavoriteActivities)
   }
 }
 </script>
 
 <style scoped>
-
+h1{
+  margin-bottom: 5px;
+}
+h2{
+  margin: 5px 0 2px 0
+}
+button{
+  padding: 5px 10px;
+  margin-right: 5px;
+  border: none;
+  border-radius: 5px;
+  background: var(--accent-color);
+}
 </style>
